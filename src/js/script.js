38a31f75a5d3425978c6261168332e736e8cddfd
Viktor <m>
2021-04-16 16:13:55 +0300

    final version with adaptation and new slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,19 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        dots: true,
+        adaptiveHeight: true,
+    /* }); */
         responsive: [
             {
-                breakpoint: 1100,
+                breakpoint: 992,
                 settings: {
                     variableWidth: true,
                     dots: true,
-                    arrows: false
+                    arrows: false,
+
                 }
-            }
+            },
         ]
     });
 
